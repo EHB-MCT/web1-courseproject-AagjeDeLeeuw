960e8d22b02719500a94b629a6a2034ad1e4050b
AagjeDeLeeuw <aagje.de.leeuw@student.ehb.be>
2024-12-23 01:15:03 +0100

    Course project-Aagje De Leeuw-Timeline, footer, hamburgermenu en responsive gemaakt

---
+++ script.js
@@ -0,0 +1,8 @@
+function menu() {
+	let navMenu = document.getElementById("nav-menu");
+	if (navMenu.style.display === "flex") {
+		navMenu.style.display = "none";
+	} else {
+		navMenu.style.display = "flex";
+	}
+}
